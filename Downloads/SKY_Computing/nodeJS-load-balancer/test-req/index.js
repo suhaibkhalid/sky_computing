7886212d2b40205d3f2831fdae59b53fbddcb81d
suhaibkhalid <suhaibsek@gamil.com>
2025-07-26 14:43:35 +0200

    chnage volumn in docker-compose file

--- Downloads/SKY_Computing/nodeJS-load-balancer/test-req/index.js
+++ Downloads/SKY_Computing/nodeJS-load-balancer/test-req/index.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
 
 for (let i = 1; i <= 20; i++) {
-  axios.post('http://localhost/', { reqNo: i })
+  //axios.post('http://localhost/', { reqNo: i })
+  axios.post('http://13.60.56.58/', { reqNo: i })
     .then(res => {
       console.log(`Request ${i} handled by ${res.data.source}`);
     })
